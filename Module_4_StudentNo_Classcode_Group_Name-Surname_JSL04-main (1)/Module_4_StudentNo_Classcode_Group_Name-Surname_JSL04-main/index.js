11a5726b89eb3ccef61af2a4ee40530e59cd2a8c
Rethabile Diale <rethadiale27@gmail.com>
2024-03-19 14:06:23 +0200

    commit

--- Module_4_StudentNo_Classcode_Group_Name-Surname_JSL04-main (1)/Module_4_StudentNo_Classcode_Group_Name-Surname_JSL04-main/index.js
+++ Module_4_StudentNo_Classcode_Group_Name-Surname_JSL04-main (1)/Module_4_StudentNo_Classcode_Group_Name-Surname_JSL04-main/index.js
@@ -42,13 +42,13 @@ console.log(currentTemp <= 5 ? 'Chilled to perfection!' : 'Needs a cooler!');
 
 // Define the current hour in 24-hour format
 let currentHour = 22;
-let drinkDecision = currentHour >= 7 && currentHour < 24 ? "Unleash the Beast!" : "Better stick to water.";
-console.log(currentHour >= 7 && currentHour < 24 ? 'Unleash the Beast!' : 'Better stick to water.');
+
 
 // Write your code below this line to determine if it's a good idea to have a Monster Energy drink based on the current hour
 // If it's between 7am and midnight (24-hour format), print "Unleash the beast!"
 // Otherwise, print "Better stick to water."
-
+let drinkDecision = currentHour >= 7 && currentHour < 24 ? "Unleash the Beast!" : "Better stick to water.";
+console.log(currentHour >= 7 && currentHour < 24 ? 'Unleash the Beast!' : 'Better stick to water.');
 
 
 
